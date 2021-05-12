f3d3da9f2758824b9354270eeac0e24647c024d4
RanjuRanjith <48095817+RanjuRanjith@users.noreply.github.com>
2021-05-12 16:41:09 +0530

    Update script.js

--- assignment4/script.js
+++ assignment4/script.js
@@ -1,13 +1,13 @@
 var names=new Array();
-names[0]="Ranju";
-names[1]="jason";
+names[0]="Yaakov";
+names[1]="John";
 names[2]="Jen";
-names[3]="John";
-names[4]="kaul";
+names[3]="jason";
+names[4]="paul";
 names[5]="frank";
-names[6]="harry";
+names[6]="larry";
 names[7]="paula";
-names[8]="raki";
+names[8]="laura";
 names[9]="jim";
 
 
